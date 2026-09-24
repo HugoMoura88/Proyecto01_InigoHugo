8ac4811e5c7e0069090f853ec7ed0a9ecb7e0cff
Inigo <garciabelenguerinigo@gmail.com>
2026-09-24 18:52:06 +0200

    menu del main

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,10 +4,56 @@
  */
 package app;
 
+
+import java.util.Scanner;
+
 /**
  *
  * @author 2DAM
  */
-public class Main {
+public static void main(String[] args)  {
     
-}
+    Scanner sc = new Scanner(System.in);
+   
+		System.out.println("""
+				=======================================
+
+				               MENU
+				     1. Ej 1          2. Ej 2
+				     3. Ej 3          4. Ej 4
+				     5. Ej 5          6. Ej 6
+				     7. Ej 7          8. Ej 8
+
+				======================================""");
+		System.out.println("Dime numero de ejercicio:");
+		String ejercicio = sc.nextLine();
+		int numero = Integer.parseInt(ejercicio);
+		switch (numero) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+
+		default:
+			throw new IllegalArgumentException("No exixste ese ejercicio" );
+			
+		}
+
+	}
+
+
+
+    
+

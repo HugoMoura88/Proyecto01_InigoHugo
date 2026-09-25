ab4562e874f45f1043f2fd5fbb3ada4be1503b35
Inigo <garciabelenguerinigo@gmail.com>
2026-09-25 17:25:21 +0200

    ruta menus

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,59 +1,95 @@
 package app;
+
 /*package app;*/
-
-
 import java.util.Scanner;
 
 /**
  *
  * @author 2DAM
  */
-public class Main {
-public static void main(String[] args)  {
-    
-    Scanner sc = new Scanner(System.in);
-   
-		System.out.println("""
-				============================================================
-				               ------------MENU-----------
-				1. Todos los libros          2. Buscar por titulo
-				3. Buscar por autor          4. Buscar por rango de precios
-				5. Buscar por stock minimo   6. Insertar libro
-				7. Borrar libro          
-				============================================================""");
-		System.out.println("Opcion: ");
-		String ejercicio = sc.next();
-		int numero = Integer.parseInt(ejercicio);
-		switch (numero) {
-		case 1:
-			Funciones.mostrarTodosLosLibros();
-                        break;
-		case 2:
-			Funciones.buscarPorTitulo();
-                        break;
-		case 3:
-			Funciones.buscarPorAutor();
-                        break;
-		case 4:
-			Funciones.buscarPorRangoPrecios();
-                        break;
-		case 5:
-			Funciones.buscarPorStockMinimo();
-                        break;
-		case 6:
-			Funciones.insertarLibros();
-                        break;
-		case 7:
-			Funciones.eliminarLibro();
-                        break;
-		case 8:
-			break;
+  
 
-		default:
-			throw new IllegalArgumentException("No exixste ese ejercicio" );
-			
-		}
+    public class Main {
 
-	}
+        public static void main(String[] args) {
 
-} 
+            menuMySQL();
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("""
+                             ============================================================
+                                            ------------Selecciona Origen-----------
+                                                      1.CSV         2. MySQL
+                             ============================================================""");
+            System.out.println("Opcion:");
+            int opcion = scanner.nextInt();
+            switch (opcion) {
+
+                case 1:
+                    menuCSV();
+                    break;
+
+                case 2:
+                    menuMySQL();
+                    break;
+
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        }
+    }
+
+    public static void mainn() {
+             Scanner scanner = new Scanner(System.in);
+            
+             System.out.println("Dime la ruta -> ");
+             String ruta = scanner.nextLine();
+             ruta=ruta.replace("\\","/");
+            
+              
+    }
+
+    public static void menuMySQL() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("""
+                             ============================================================
+                                            ------------MENU-----------
+                             1. Todos los libros          2. Buscar por titulo
+                             3. Buscar por autor          4. Buscar por rango de precios
+                             5. Buscar por stock minimo   6. Insertar libro
+                             7. Borrar libro
+                             ============================================================""");
+        System.out.println("Opcion: ");
+        String ejercicio = sc.next();
+        int numero = Integer.parseInt(ejercicio);
+        switch (numero) {
+            case 1:
+                Funciones.mostrarTodosLosLibros();
+                break;
+            case 2:
+                Funciones.buscarPorTitulo();
+                break;
+            case 3:
+                Funciones.buscarPorAutor();
+                break;
+            case 4:
+                Funciones.buscarPorRangoPrecios();
+                break;
+            case 5:
+                Funciones.buscarPorStockMinimo();
+                break;
+            case 6:
+                Funciones.insertarLibros();
+                break;
+            case 7:
+                Funciones.eliminarLibro();
+                break;
+            case 8:
+                break;
+
+            default:
+                throw new IllegalArgumentException("No exixste ese ejercicio");
+
+        }
+    }

5774d7cff0edde8ea358379ee8dbf0f9a9c85d32
Hugo <hugompp178@gmail.com>
2026-09-25 18:19:01 +0200

    Csv implementado a funciones

--- src/app/Funciones.java
+++ src/app/Funciones.java
@@ -5,6 +5,7 @@
 package app;
 
 import dao.LibroDAO;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import modelo.Libro;
@@ -15,65 +16,100 @@ import modelo.Libro;
  */
 public class Funciones {
 //1. Mostrar todos los libros: mostrará por pantalla todos los libros disponibles en el sistema.
-    public static void mostrarTodosLosLibros() {
+
+    public static void mostrarTodosLosLibros(String csv) {
         System.out.println("Libros:");
         LibroDAO dao = new LibroDAO();
-        List<Libro> libros = dao.obtenerTodos();
+        List<Libro> libros = new ArrayList<Libro>();
+        if (csv.equals("")) {
+            libros = dao.obtenerTodos();
+        } else {
+            libros = util.FromToTxt.obtenerTodosTxt(csv);
+        }
         for (Libro libro : libros) {
             System.out.println(libro);
-            
+
         }
     }
- //2. Buscar libro por título: permite buscar un libro específico por su título.
-    public static void buscarPorTitulo() {
+    //2. Buscar libro por título: permite buscar un libro específico por su título.
+
+    public static void buscarPorTitulo(String csv) {
         Scanner sc = new Scanner(System.in);
         LibroDAO dao = new LibroDAO();
         System.out.println("Titulo: ");
         String titulo = sc.next();
-        System.out.println(dao.obtenerPorTitulo(titulo));
-        
+        if (csv.equals("")) {
+            System.out.println(dao.obtenerPorTitulo(titulo));
+        } else {
+            System.out.println(util.FromToTxt.buscarPorTitulo(csv, titulo));
+        }
     }
- //3. Buscar libros por autor: permite buscar libros de un autor específico.
-    public static void buscarPorAutor() {
+    //3. Buscar libros por autor: permite buscar libros de un autor específico.
+
+    public static void buscarPorAutor(String csv) {
         Scanner sc = new Scanner(System.in);
         LibroDAO dao = new LibroDAO();
         System.out.println("Autor: ");
         String autor = sc.next();
-        System.out.println(dao.obtenerPorAutor(autor));
-        
+        if (csv.equals("")) {
+            System.out.println(dao.obtenerPorAutor(autor));
+        } else {
+            System.out.println(util.FromToTxt.buscarPorAutor(csv, autor));
+        }
+
     }
 //4. Buscar libros por rango de precios: permite buscar libros dentro de un rango de precios indicado por el usuario.
-    public static void buscarPorRangoPrecios() {
+
+    public static void buscarPorRangoPrecios(String csv) {
         Scanner sc = new Scanner(System.in);
         LibroDAO dao = new LibroDAO();
-        List<Libro> libros = dao.obtenerTodos();
+        List<Libro> libros = new ArrayList<Libro>();
         System.out.println("Rango de precios: ");
         System.out.println("De: ");
         Double precio1 = Double.valueOf(sc.next());
         System.out.println("Hasta: ");
         Double precio2 = Double.valueOf(sc.next());
-        for (Libro libro : libros) {
-            if (libro.getPrecio()>precio1&&libro.getPrecio()<precio2){
+        if (csv.equals("")) {
+            libros = dao.obtenerTodos();
+            for (Libro libro : libros) {
+                if (libro.getPrecio() > precio1 && libro.getPrecio() < precio2) {
+                    System.out.println(libro);
+                }
+            }
+        } else {
+            libros = util.FromToTxt.buscarPorRangoPrecios(csv, precio1, precio2);
+            for (Libro libro : libros) {
                 System.out.println(libro);
             }
         }
+
     }
 //5. Buscar libros por cantidad mínima en stock: permite buscar libros con stock igual o mayor al especificado.
-    public static void buscarPorStockMinimo() {
+
+    public static void buscarPorStockMinimo(String csv) {
         Scanner sc = new Scanner(System.in);
         LibroDAO dao = new LibroDAO();
-        List<Libro> libros = dao.obtenerTodos();
+        List<Libro> libros = new ArrayList<Libro>();
         System.out.println("Stock minimo: ");
         int stock = Integer.parseInt(sc.next());
-        for (Libro libro : libros) {
-            if (libro.getStock()>=stock ){
+        if (csv.equals("")) {
+            libros = dao.obtenerTodos();
+            for (Libro libro : libros) {
+                if (libro.getStock() >= stock) {
+                    System.out.println(libro);
+                }
+            }
+        } else {
+            libros = util.FromToTxt.buscarPorStockMinimo(csv, stock);
+            for (Libro libro : libros) {
                 System.out.println(libro);
             }
         }
-        
+
     }
 //6. Insertar nuevo libro: el usuario proporcionará id, título, autor, precio y stock del nuevo libro.
-    public static void insertarLibros() {
+
+    public static void insertarLibros(String csv) {
         LibroDAO dao = new LibroDAO();
         Scanner sc = new Scanner(System.in);
         System.out.println("Insertar nuevo libro: ");
@@ -87,33 +123,52 @@ public class Funciones {
         Double precio = Double.valueOf(sc.next());
         System.out.println("Stock: ");
         int stock = Integer.parseInt(sc.next());
-        Libro libro = new Libro(id,titulo,autor,precio,stock);
-        System.out.println(dao.insertar(libro));
+        Libro libro = new Libro(id, titulo, autor, precio, stock);
+        if (csv.equals("")) {
+            System.out.println(dao.insertar(libro));
+        } else {
+            util.FromToTxt.insertarNuevoLibro(csv, libro);
+        }
+
     }
-        
+
 //7. Eliminar libro por título: elimina un libro por su título. Si hay varios con el mismo título, el usuario elige por id.
-            public static void eliminarLibro() {
+    public static void eliminarLibro(String csv) {
         LibroDAO dao = new LibroDAO();
-        List<Libro> libros = dao.obtenerTodos();
+        List<Libro> libros = new ArrayList<Libro>();
         Scanner sc = new Scanner(System.in);
         System.out.println("Eliminar libro: ");
         System.out.println("Titulo: ");
         String titulo = sc.next();
         int con = 0;
+        if (csv.equals("")) {
+            libros = dao.obtenerTodos();
+        } else {
+            libros = util.FromToTxt.obtenerTodosTxt(csv);
+        }
         for (Libro libro : libros) {
-            if (libro.getTitulo().equals(titulo)){
+            if (libro.getTitulo().equals(titulo)) {
                 System.out.println(libro);
                 con++;
             }
         }
-        if (con>1){
+        if (con > 1) {
             System.out.println("Hay varios libros con ese titulo, escribe la id del que quieres eliminar: ");
             String id = sc.next();
-            System.out.println(dao.eliminarPorId(id));
-        } else {
-            System.out.println(dao.eliminarPorTitulo(titulo));
-        }
+            if (csv.equals("")) {
+                System.out.println(dao.eliminarPorId(id));
+            } else {
+                util.FromToTxt.eliminarLibroPorId(csv,id);
             }
-        
+        } else {
+            if (csv.equals("")) {
+                System.out.println(dao.eliminarPorTitulo(titulo));
+            } else {
+                util.FromToTxt.eliminarLibroPorTitulo(csv,titulo);
+            }
+            
+        }
+    }
+
 //8. Hacer copia: copia todos los datos del repositorio activo al otro (de archivo a MySQL o viceversa).      
-    }       
+}
--- src/util/FromToTxt.java
+++ src/util/FromToTxt.java
@@ -17,7 +17,7 @@ import modelo.Libro;
  * @author 2DAM
  */
 public class FromToTxt {
-    
+
     public static List<Libro> obtenerTodosTxt(String txt) {
         List<Libro> libros = new ArrayList<Libro>();
         Scanner leer = null;
@@ -30,16 +30,16 @@ public class FromToTxt {
             }
         } catch (Exception e) {
         }
-        
+
         for (String linea : lineas) {
             String[] lineaSeparada = linea.split(",");
-            libros.add(new Libro(lineaSeparada[0],lineaSeparada[1],lineaSeparada[2],Double.valueOf(lineaSeparada[3]),Integer.parseInt(lineaSeparada[4])));
+            libros.add(new Libro(lineaSeparada[0], lineaSeparada[1], lineaSeparada[2], Double.valueOf(lineaSeparada[3]), Integer.parseInt(lineaSeparada[4])));
         }
         return libros;
-        
+
     }
-    
-    public static List<Libro> buscarPorTitulo(String txt,String titulo) {
+
+    public static List<Libro> buscarPorTitulo(String txt, String titulo) {
         List<Libro> libros = obtenerTodosTxt(txt);
         List<Libro> ret = new ArrayList<Libro>();
         for (Libro libro : libros) {
@@ -49,8 +49,8 @@ public class FromToTxt {
         }
         return ret;
     }
-    
-    public static List<Libro> buscarPorAutor(String txt,String autor) {
+
+    public static List<Libro> buscarPorAutor(String txt, String autor) {
         List<Libro> libros = obtenerTodosTxt(txt);
         List<Libro> ret = new ArrayList<Libro>();
         for (Libro libro : libros) {
@@ -60,70 +60,89 @@ public class FromToTxt {
         }
         return ret;
     }
-    
-     public static List<Libro> buscarPorRangoPrecios(String txt,Double m, Double ma) {
+
+    public static List<Libro> buscarPorRangoPrecios(String txt, Double m, Double ma) {
         List<Libro> libros = obtenerTodosTxt(txt);
         List<Libro> ret = new ArrayList<Libro>();
         for (Libro libro : libros) {
-            if (libro.getPrecio() >= m ||libro.getPrecio() < ma) {
+            if (libro.getPrecio() >= m || libro.getPrecio() < ma) {
                 ret.add(libro);
             }
         }
         return ret;
     }
-     
-     public static List<Libro> buscarPorStockMinimo(String txt,int stock) {
+
+    public static List<Libro> buscarPorStockMinimo(String txt, int stock) {
         List<Libro> libros = obtenerTodosTxt(txt);
         List<Libro> ret = new ArrayList<Libro>();
         for (Libro libro : libros) {
-            if (libro.getStock()>= stock) {
+            if (libro.getStock() >= stock) {
                 ret.add(libro);
             }
         }
         return ret;
     }
-     
-     public static void insertarNuevoLibro(String txt,Libro objeto) {
+
+    public static void insertarNuevoLibro(String txt, Libro objeto) {
         List<Libro> libros = obtenerTodosTxt(txt);
         PrintWriter es = null;
-        String linea = "";
         Boolean esta = false;
         try {
             es = new PrintWriter(new FileWriter(txt));
             es.println("idlibros,titulo,autor,precio,stock");
             for (Libro libro : libros) {
-            es.println(libro.getId()+","+libro.getTitulo()+","+libro.getAutor()+","+libro.getPrecio()+","+libro.getStock());
-            if ( libro.getId().equals(objeto.getId())){
-                esta = true;
-            }
+                es.println(libro.getId() + "," + libro.getTitulo() + "," + libro.getAutor() + "," + libro.getPrecio() + "," + libro.getStock());
+                if (libro.getId().equals(objeto.getId())) {
+                    esta = true;
+                }
             }
             if (esta) {
                 System.out.println("Ya hay un libro con la misma id");
             } else {
-                es.println(objeto.getId()+","+objeto.getTitulo()+","+objeto.getAutor()+","+objeto.getPrecio()+","+objeto.getStock());
+                es.println(objeto.getId() + "," + objeto.getTitulo() + "," + objeto.getAutor() + "," + objeto.getPrecio() + "," + objeto.getStock());
             }
-            
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		es.close();
-     }
-        
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        es.close();
+    }
+
+    public static void eliminarLibroPorTitulo(String txt, String titulo) {
+        List<Libro> libros = obtenerTodosTxt(txt);
+        PrintWriter es = null;
+        Boolean esta = false;
+        try {
+            es = new PrintWriter(new FileWriter(txt));
+            es.println("idlibros,titulo,autor,precio,stock");
+            for (Libro libro : libros) {
+                if (!(libro.getTitulo().equals(titulo))) {
+                    es.println(libro.getId() + "," + libro.getTitulo() + "," + libro.getAutor() + "," + libro.getPrecio() + "," + libro.getStock());
+                }
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        es.close();
+    }
+    public static void eliminarLibroPorId(String txt, String id) {
+        List<Libro> libros = obtenerTodosTxt(txt);
+        PrintWriter es = null;
+        Boolean esta = false;
+        try {
+            es = new PrintWriter(new FileWriter(txt));
+            es.println("idlibros,titulo,autor,precio,stock");
+            for (Libro libro : libros) {
+                if (!(libro.getId().equals(id))) {
+                    es.println(libro.getId() + "," + libro.getTitulo() + "," + libro.getAutor() + "," + libro.getPrecio() + "," + libro.getStock());
+                }
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        es.close();
+    }
     
-     
-     
-     
     
-     
-     
-     
-    
-    
-    
-    
-    
-   
-    
-    
-    
+
 }
